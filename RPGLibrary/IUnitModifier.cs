9d57aa0b297ccb488ed62832c4a692c61f8e6d34
RiceGnat <ricegnat@gmail.com>
2017-02-23 11:12:38 -0600

    test commit

--- RPGLibrary/IUnitModifier.cs
+++ RPGLibrary/IUnitModifier.cs
@@ -6,9 +6,13 @@ using System.Threading.Tasks;
 
 namespace RPGLibrary
 {
+	/// <summary>
+	/// Exposes properties of unit modifiers.
+	/// </summary>
 	public interface IUnitModifier : IUnit
 	{
 		IUnit Base { get; }
+
 		void Bind(IUnit target);
 	}
 }
