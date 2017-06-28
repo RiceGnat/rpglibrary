a93848de2d9d47c41b96f2e4bc7cb5cf54347589
RiceGnat <ricegnat@gmail.com>
2017-06-28 15:48:36 -0500

    imported CircularLinkedList and Serializer from old project

---
+++ RPGLibrary/Collections/ICircularLinkedList(T).cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace RPGLibrary.Collections.Generic
+{
+	public interface ICircularLinkedList<T> : IList<T>
+	{
+		T Current { get; }
+		void Rotate();
+	}
+}
