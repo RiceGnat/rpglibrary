1c76f7a3c474f540dfe0f6293f97e9579646995b
RiceGnat <ricegnat@gmail.com>
2017-07-11 17:10:29 -0500

    added Spell class

---
+++ Davfalcon/ISpell.cs
@@ -0,0 +1,16 @@
+﻿namespace Davfalcon
+{
+	public interface ISpell
+	{
+		Element SpellElement { get; }
+		SpellTargetType TargetType { get; }
+		int Cost { get; }
+		int BaseDamage { get; }
+		int BaseHeal { get; }
+		int Range { get; }
+		int Size { get; }
+		int MaxTargets { get; }
+
+		event SpellEventHandler OnCast;
+	}
+}
---
+++ Davfalcon/Spell.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using  RPGLibrary;
+
+namespace Davfalcon
+{
+	public enum SpellTargetType
+	{
+		Self, Target, Area, Line, Attack
+	}
+
+	public delegate void SpellEventHandler(IUnit caster, ISpell spell, params IUnit[] targets);
+
+	[Serializable]
+	public class Spell : ISpell
+	{
+		public Element SpellElement { get; set; }
+		public SpellTargetType TargetType { get; set; }
+		public int Cost { get; set; }
+		public int BaseDamage { get; set; }
+		public int BaseHeal { get; set; }
+		public int Range { get; set; }
+		public int Size { get; set; }
+		public int MaxTargets { get; set; }
+
+		public event SpellEventHandler OnCast;
+	}
+}
