a8c8976717de83787c4ebf18624c7712ec93f666
RiceGnat <ricegnat@gmail.com>
2017-10-22 05:34:25 -0500

    SpellCastOptions

---
+++ Davfalcon/Combat/SpellCastOptions.cs
@@ -0,0 +1,14 @@
+﻿namespace Davfalcon.Combat
+{
+	public class SpellCastOptions
+	{
+		public int AdjustedCost { get; set; }
+		public int DamageMultiplier { get; set; }
+
+		public SpellCastOptions()
+		{
+			AdjustedCost = -1;
+			DamageMultiplier = -1;
+		}
+	}
+}
