222cf72ffc00c629d10028d76a5179e399bc56a8
RiceGnat <ricegnat@gmail.com>
2017-11-05 22:00:16 -0600

    unit test project

---
+++ RPGLibraryUnitTests/BasicUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RPGLibrary;
+
+namespace RPGLibraryUnitTests
+{
+	[TestClass]
+	public class BasicUnitTest
+	{
+		private const string NAME = "Test unit";
+		private const string CLASS = "Test class";
+		private const int LEVEL = 1;
+
+		[TestMethod]
+		public void ConstructorTest()
+		{
+			IUnit unit = new BasicUnit
+			{
+				Name = NAME,
+				Class = CLASS,
+				Level = LEVEL
+			};
+
+			Assert.AreEqual(NAME, unit.Name);
+			Assert.AreEqual(CLASS, unit.Class);
+			Assert.AreEqual(LEVEL, unit.Level);
+			Assert.IsNotNull(unit.Stats);
+		}
+	}
+}
