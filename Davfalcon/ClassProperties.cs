90ced5c6a5fa012f2571837267f45e8d6fb9a626
RiceGnat <ricegnat@gmail.com>
2017-11-10 13:52:23 -0600

    class properties

---
+++ Davfalcon/ClassProperties.cs
@@ -0,0 +1,16 @@
+﻿using RPGLibrary;
+
+namespace Davfalcon
+{
+	public class ClassProperties
+	{
+		public string DisplayName { get; set; }
+		public string Description { get; set; }
+		public IStatsEditable StatGrowths { get; private set; }
+
+		public ClassProperties()
+		{
+			StatGrowths = new StatsMap();
+		}
+	}
+}
