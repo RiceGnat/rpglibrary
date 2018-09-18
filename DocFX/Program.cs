d621386818c4d43c66e16034576c5687a3084c76
RiceGnat <ricegnat@gmail.com>
2018-09-17 19:38:32 -0500

    added docfx

---
+++ DocFX/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace DocFX
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+			WebHost.CreateDefaultBuilder(args)
+				.UseStartup<Startup>()
+				.UseWebRoot(Path.Combine(Directory.GetCurrentDirectory(), "..", "docs"));
+	}
+}
