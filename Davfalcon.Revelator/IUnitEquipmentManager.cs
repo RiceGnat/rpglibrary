191c654c5d1a4b10b8467b32dd5c9c261293269c
RiceGnat <ricegnat@gmail.com>
2019-10-10 17:27:38 -0500

    started reimplementing equipment manager

---
+++ Davfalcon.Revelator/IUnitEquipmentManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Davfalcon.Revelator
+{
+	public interface IUnitEquipmentManager<TUnit, TEquipSlots> : IModifier<TUnit>
+        where TUnit : IUnitTemplate<TUnit>
+        where TEquipSlots : Enum
+	{
+		IEnumerable<TEquipSlots> EquipmentSlots { get; }
+		IEnumerable<IEquipment<TUnit, TEquipSlots>> All { get; }
+
+		void AddEquipmentSlot(TEquipSlots slotType);
+        void AddEquipmentSlot(TEquipSlots slotType, int capacity);
+        IEquipment<TUnit, TEquipSlots> GetEquipment(TEquipSlots slot);
+		IEquipment<TUnit, TEquipSlots> GetEquipment(TEquipSlots slotType, int offset);
+		IEnumerable<IEquipment<TUnit, TEquipSlots>> GetAllEquipmentForSlot(TEquipSlots slot);
+		void Equip(IEquipment<TUnit, TEquipSlots> equipment);
+		void Equip(IEquipment<TUnit, TEquipSlots> equipment, int offset);
+		void EquipSlotIndex(IEquipment<TUnit, TEquipSlots> equipment, int index);
+		void UnequipSlot(TEquipSlots slot);
+		void UnequipSlot(TEquipSlots slotType, int offset);
+		void UnequipSlotIndex(int index);
+	}
+}
